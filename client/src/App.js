57f064c7e68eb6e04f27e86d5e38bf306dda8dce
Mfrancois3k <mfrancois3k.com>
2020-01-14 19:45:07 -0500

    1st comment for project

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className="particles"
+                params={particlesOptions } />
+        
+        <Navigation />
+         <Logo />
+          <Rank />
+         {/* <ImageLinkForm />
+         import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+  <FaceRecognition /> */}
+       
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/Components/ImageLinkFrom/ImageLinkForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+        <p className="f3"> {'This Magic Brain will detect faces in your pictures. Give it a try '} </div>
+        <div className="center"> 
+        <div className="form center pa4 br3 shadow-5">
+        <input className="f4 pa2 w-70 center" type='tex' />
+        <div className="w-30 grow f4 link ph3 pv2 dib wight bg-light">
+        </div>
+       </div> 
+      </div>
+        
+    );
+}
+
+export default ImageLinkForm;
